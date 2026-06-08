edbbaa1f8c4e74ccdbdce62906eef0447e956c93
Mithun P <mithunparivallal@gmail.com>
2026-06-08 11:32:08 +0530

    Add files via upload

---
+++ track 2/Day8/from sklearn.feature_extraction.py
@@ -0,0 +1,4 @@
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.model_selection import GridSearchCV
+from sklearn.pipeline import Pipeline
+from sklearn.neighbors import NearestNeighbors
